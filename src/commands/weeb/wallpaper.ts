fa7e52e7d4eb146563ec0ec881b4ee6621258b90
deku <89695172+saddeku@users.noreply.github.com>
2021-08-28 20:24:38 +0530

    Update wallpaper.ts

--- src/commands/weeb/wallpaper.ts
+++ src/commands/weeb/wallpaper.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
         const rnekolc = rnekol[Math.floor(Math.random() * rnekol.length)];
         const neko = await axios.get('https://nekos.life/api/v2/img/' + rnekolc)
 
-return void M.reply(await request.buffer(neko.data.url), MessageType.image, undefined, undefined, `*here you go ✨*`)
+return void M.reply(await request.buffer(neko.data.url), MessageType.image, undefined, undefined, `*here you go 🖤*`)
 
 
 
