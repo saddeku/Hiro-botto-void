6146391adbb725f0138375f54f290edc1c3794ef
deku <89695172+saddeku@users.noreply.github.com>
2021-08-28 20:32:25 +0530

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
                 videos[i].author.name
             }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
         }
-        M.reply(' 💫 searching...')
+        M.reply(' 🖤 searching...')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
