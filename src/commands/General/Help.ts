e17960d459f10e1700fb7355630a884a202a98ea
deku <89695172+saddeku@users.noreply.github.com>
2021-08-28 20:33:58 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `✨ *Star's Command List* ✨\n\n`
+            let text = `🖤 *Deku's Command List* 🖤\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
