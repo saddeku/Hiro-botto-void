b5646f5fa344ac50136acccba81fa06a66f1ad40
deku <89695172+saddeku@users.noreply.github.com>
2021-08-28 20:41:45 +0530

    Update did_you_mean.ts

--- src/commands/General/did_you_mean.ts
+++ src/commands/General/did_you_mean.ts
@@ -21,11 +21,11 @@ export default class Command extends BaseCommand {
 
 
 
-return void M.reply(await request.buffer('https://i.ibb.co/4MPHYT6/baka.jpg'),
+return void M.reply(await request.buffer('https://i.pinimg.com/originals/9a/f4/0a/9af40a579d935b60ace8d9e258508eab.png'),
 MessageType.image,
             undefined,
             undefined,
-            `*No such command! Baka*`
+            `*Did you mean : ₹help*`
 
 
 )
