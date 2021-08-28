139e5350555a9a31f85743b986765e468dd66069
deku <89695172+saddeku@users.noreply.github.com>
2021-08-28 20:43:15 +0530

    Update did_you_mean.ts

--- src/commands/General/did_you_mean.ts
+++ src/commands/General/did_you_mean.ts
@@ -21,7 +21,7 @@ export default class Command extends BaseCommand {
 
 
 
-return void M.reply(await request.buffer('https://i.pinimg.com/originals/9a/f4/0a/9af40a579d935b60ace8d9e258508eab.png'),
+return void M.reply(await request.buffer('https://wallpapercave.com/wp/wp2682854.jpg'),
 MessageType.image,
             undefined,
             undefined,
